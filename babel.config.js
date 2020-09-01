7736f06be2364cd00f117d6d30d03ea43ee309e4
Felix Becker <felix.b@outlook.com>
2020-09-01 19:09:12 +0200

    Try adding nullish coalescing plugin too

--- babel.config.js
+++ babel.config.js
@@ -47,8 +47,9 @@ module.exports = api => {
     ],
     plugins: [
       'babel-plugin-lodash',
-      // Webpack v4 chokes on optional chaining syntax, fix will be released with webpack v5.
+      // Webpack v4 chokes on these syntaxes, fix will be released with webpack v5.
       ['@babel/plugin-proposal-optional-chaining', { loose: true }],
+      ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: true }],
       // Node 12 (released 2019 Apr 23) supports these natively, but there seem to be issues when used with TypeScript.
       ['@babel/plugin-proposal-class-properties', { loose: true }],
     ],
